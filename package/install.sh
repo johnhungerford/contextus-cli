eec350888d2e018cbe1b29735ae30656bc3806f6
John Hungerford <jiveshungerford@gmail.com>
2024-05-27 20:56:15 -0400

    Improved install script

--- package/install.sh
+++ package/install.sh
@@ -9,18 +9,10 @@ cp $CONTEXTUS_ORIGIN $CONTEXTUS_TARGET
 
 ZSHRC_LINE="export PATH=\$PATH:$CONTEXTUS_BIN"
 
-echo $ZSHRC_LINE
-
 if grep -Fxq $ZSHRC_LINE $HOME/.zshrc
 then
     echo "PATH correction already added to .zshrc"
 else
     echo "$ZSHRC_LINE" >> $HOME/.zshrc
     echo "PATH correction added to .zshrc"
-fi
-if [[ ":$PATH:" == *":$CONTEXTUS_BIN"* ]]; then
-  echo "Contextus bin directory already in PATH"
-else
-  PATH=$PATH:$CONTEXTUS_BIN
-  echo "Contextus bin directory added to PATH"
 fi
